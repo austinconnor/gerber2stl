3f75fa6a9e64d71563d98b59d66d7e2b1fd76d53
konar <connoraustin22@gmail.com>
2022-06-03 18:21:48 -0400

    reviving this project

---
+++ main.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include "os.hh"
+#include "converter.hh"
+#include "gerber.hh"
+
+int main(int argc, char** argv){
+    std::string inpath = argv[1];
+    std::string outdir = argv[2];
+
+    if(!os::dirExists(inpath) || !os::dirExists(outdir)){ return -1; }    
+    std::vector<std::string> files = os::getFilesInDirectory(inpath);
+
+    for(auto file : files){
+        std::cout << "FILENAME: "<< file << std::endl;
+        GerberFile *gf = new GerberFile(file, outdir);
+
+        free(gf);
+    }
+
+
+    return 0;
+}
---
+++ math_utils.hh
@@ -0,0 +1,52 @@
+#pragma once
+#include <math.h>
+#include <algorithm>
+#include <vector>
+
+
+class Point{
+public:
+    float x,y;
+    Point(){};
+    Point(float x, float y);
+
+};
+
+class Line{
+public:
+    Point p1, p2;
+
+    Line(){};
+    Line(Point p1, Point p2);
+    Line(float x1, float y1, float x2, float y2);
+    bool intersects(Line line);
+
+};
+
+class Shape{
+public:
+    std::vector<Point> points;
+    std::vector<Line> lines;
+
+    Shape(std::vector<Point> points);
+    Shape setCenter(float x, float y);
+    Shape convexHull();
+
+};
+
+class MathUtils{
+public:
+    //amount of points in a circle
+    static const int circleResolution = 20;
+
+    static float Atan2(float x, float y);
+    static double RadiansToDegrees(float rads);
+    static double DegreesToRadians(float degrees);
+    static Shape GenerateCircle(float radius);
+    static Shape GenerateRectangle(float width, float height);
+    static Shape GenerateObround(float width, float height);
+    static Shape MergeShapes(std::vector<Shape> shapes);
+
+};
+
+
